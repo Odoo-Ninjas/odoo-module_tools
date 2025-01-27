b874ff95f5fda40432e9797e393ded1706b1570a
Marc Wimmer <marc@itewimmer.de>
2025-01-27 13:57:56 +0100

    Update floattools.py

--- module_tools/floattools.py
+++ module_tools/floattools.py
@@ -2,7 +2,12 @@ from odoo.tools import float_round
 from decimal import Decimal
 
 
-def float_is_same(f1, f2, precision_rounding=None, tolerance=None):
+def float_is_same(f1, f2, precision_rounding=None, tolerance=None, total_percentage_tolerance=None):
+    """
+    :param tolerance: float, if the difference between f1 and f2 is less than tolerance, return True
+    :param total_percentage_tolerance: float, if the difference between f1 and f2 is less than total_percentage_tolerance
+
+    """
     f = list(map(Decimal, [f1, f2]))
     if precision_rounding:
         digits = len(str(1/precision_rounding).split(".")[0]) - 1
@@ -22,8 +27,13 @@ def float_is_same(f1, f2, precision_rounding=None, tolerance=None):
     f = list(map(round_to_len, f))
     if f[0] == f[1]:
         return True
-    if not tolerance:
-        return False
-    diff = abs(f[0] - f[1])
-    return diff < tolerance
-
+    if tolerance:
+        diff = abs(f[0] - f[1])
+        if diff <= tolerance:
+            return True
+    if total_percentage_tolerance and f[0]:
+        diff = abs(f[0] - f[1])
+        percentage = abs(diff / f[0] * 100)
+        if percentage <= total_percentage_tolerance:
+            return True
+    return False
